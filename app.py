ce79d27e17312eeaf8059b39b7f75b388804db77
Vitor Rocha <vitorrocha-2000@hotmail.com>
2023-04-25 13:27:24 +0100

    add import myindex.py in app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import dash
 import dash_bootstrap_components as dbc
 
+from myindex import *
+
+
 # Lista contendo links para os estilos utilizados na aplicação
 estilos = ["https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
            "https://fonts.googleapis.com/icon?family=Material+Icons", dbc.themes.COSMO]
