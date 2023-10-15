aabcfd5a19692609113f1eca9085bb7842ab48e2
Vitor Rocha <vitorrocha-2000@hotmail.com>
2023-10-15 22:12:37 +0100

    [Feat]: Add methods to other banks (empty metods)

--- app/utils/statemants_transform_service.py
+++ app/utils/statemants_transform_service.py
@@ -40,3 +40,38 @@ class StatemantTransform:
         })
 
         return receitas_transformed, despesas_transformed
+    
+    def transform_cgd(self):
+        df_file_uploaded = self._decode_contents()
+
+        df_file_uploaded = df_file_uploaded.dropna(axis=1, how='all') ## drop columns with all NaN values
+
+        receitas_file_uploaded = df_file_uploaded[df_file_uploaded['Tipo'] == 'Entrada']
+        despesas_file_uploaded = df_file_uploaded[df_file_uploaded['Tipo'] == 'Saída']
+
+        receitas_transformed = pd.DataFrame({
+            'Valor': receitas_file_uploaded['Valor'],
+            'Moeda': receitas_file_uploaded['Moeda de destino'], ## 'Moeda de origem' or 'Moeda de destino
+            'Efetuado': 1,
+            'Fixo': 0,
+            'Data': pd.to_datetime(receitas_file_uploaded['Concluída em']),
+            'Categoria': 'Outros',
+            'Descrição': receitas_file_uploaded['Nome de origem']
+        })
+
+        despesas_transformed = pd.DataFrame({
+            'Valor': despesas_file_uploaded['Valor'],
+            'Moeda': despesas_file_uploaded['Moeda de destino'],
+            'Efetuado': 1,
+            'Fixo': 0,
+            'Data': despesas_file_uploaded['Concluída em'],
+            'Categoria': 'Outros',
+            'Descrição': despesas_file_uploaded['Nome do beneficiário']
+        })
+
+        # return receitas_transformed, despesas_transformed
+        return None, None
+    
+    def transform_nubank(self): ...
+    def transform_inter(self): ...
+
